82507bb557e986f87a97e45afecb8d57f04db77e
Darren Griffin <darren@klsuk.local>
2016-10-05 16:13:43 +0100

    Removed old comment

--- classes/API.class.php
+++ classes/API.class.php
@@ -5,8 +5,6 @@
 		public $shopUrl;
 		public $apiKey;
 		
-		//http://shop.klsonline.co.uk/api/products?ws_key=E7SDRHYGVW4U2S1JNXXILAFQRG2GRXY6&output_format=JSON&limit=1
-		
 		public function __construct($shopUrl,$key){
 			$this->shopUrl = $shopUrl;
 			$this->apiKey = $key;

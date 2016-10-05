57cb97c692ada4501e2594708063d2608d31fb30
Darren Griffin <darren@klsuk.local>
2016-10-05 16:17:44 +0100

    Cleaned up unused files and cleaned code

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <div class="container">
 	<?php require 'parts/header.php'; ?>
 	<div class="jumbotron">
-  <h1>CSV Creator</h1>
-  <p>Converts a CSV file to a compatible format for the PrestaShop Import tool</p>
-  <p><span style="font-size:14px;">Created by Darren Griffin</span></p>
-</div>
+		<h1>CSV Creator</h1>
+		<p>Converts a CSV file to a compatible format for the PrestaShop Import tool</p>
+		<p><span style="font-size:14px;">Created by Darren Griffin</span></p>
+	</div>
 	<?php require 'parts/footer.php'; ?>
 </div>
 
